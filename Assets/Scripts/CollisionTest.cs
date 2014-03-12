c9c3fadfd826e79aa77b55c0afbdea866e19fa5e
Daniel Jallov <danieljallov@gmail.com>
2014-03-12 12:21:55 +0100

    Set up simple collision detection

---
+++ Assets/Scripts/CollisionTest.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CollisionTest : MonoBehaviour {
+
+    List<Collider> currentlyHitting;
+
+	// Use this for initialization
+	void Start () {
+        print("Yo");
+        currentlyHitting = new List<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        print("Enter trigger");
+
+        var direction = other.transform.position - transform.position;
+        direction.y = 0;
+        direction.Normalize();
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, direction, out hit, 100f))
+        {
+            if (hit.collider.Equals(other))
+            {
+                print("Hit the target");
+            }
+
+        }
+
+        if (!currentlyHitting.Contains(other))
+        {
+            currentlyHitting.Add(other);
+        }
+        print("Currently hitting " + currentlyHitting.Count);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        print("Exit trigger");
+
+        currentlyHitting.Remove(other);
+
+        print("Currently hitting " + currentlyHitting.Count);
+    }
+}
---
+++ Assets/Scripts/SimpleMoveScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMoveScript : MonoBehaviour {
+
+    public GameObject Target;
+    public float MoveSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Target != null) {
+            var direction = Vector3.right;
+            direction.y = 0;
+            direction.Normalize();
+
+            transform.position = transform.position + direction * MoveSpeed;
+        }
+	}
+}
